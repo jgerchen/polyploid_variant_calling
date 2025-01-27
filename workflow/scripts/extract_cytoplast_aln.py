f033e788c870c2cf8bf2f3d43183de83b139697a
Ekaterina Pulko <ekaterinapulko@gmail.com>
2025-01-27 11:06:41 +0100

    improved version

--- workflow/scripts/extract_cytoplast_aln.py
+++ workflow/scripts/extract_cytoplast_aln.py
@@ -6,7 +6,7 @@ min_depth=5
 #name of contigs we want to look at, e.g. for cpDNA
 sample_contigs=["OY340914.1"]
 #name of the output file
-output_aln="cp_alignments.fasta"
+output_aln="new_best.fasta"
 #dictionary to hold results
 aln_dict={}
 with open(sample_table) as sample_files:
@@ -24,19 +24,32 @@ with open(sample_table) as sample_files:
             bam_pileup=bam_input.pileup(sample_contig)
             record_depth_sum=0
             #iterate over each column
+            counter = 0
+            last_pos = -1
             for pileup_pos in bam_pileup:
+                # counter += 1
+                # if counter == 1000:
+                #     break
+                if last_pos != pileup_pos.reference_pos - 1:
+                    if last_pos != -1:
+                        # if part of the reference was skipped
+                        aln_string += "N" * (pileup_pos.reference_pos - last_pos + 1)
+                last_pos = pileup_pos.reference_pos
                 #test if we have minimum depth at position, otherwise just add N to output sequence
-                if pileup_pos.n<=min_depth:
-                    aln_string=aln_string+"N"
-                else:
-                    pos_count={"A":0, "C":0, "G":0, "T":0, "N":0}
-                    #Here is the tricky part, I think the refskip part is more complex
-                    for pileupread in pileup_pos.pileups:
-                        if pileupread.is_del or pileupread.is_refskip:
-                            pos_count["N"]+=1
+                # if pileup_pos.n<=min_depth:
+                #     aln_string=aln_string+"N"
+                # else:
+                pos_count={"A":0, "C":0, "G":0, "T":0}
+                deletion = True
+                deletion_length = 0
+                insertion_length = 0
+                for pileupread in pileup_pos.pileups:
+                        if pileupread.is_del:
+                            pass
                         else:
+                            deletion = False
                             pos_count[pileupread.alignment.query_sequence[pileupread.query_position]]+=1
-
+                if not deletion:
                     aln_string=aln_string+[w for w in sorted(pos_count, key=pos_count.get, reverse=True)][0]
         aln_dict.update({sample_name:aln_string})
 #write results for all samples to one fasta file
